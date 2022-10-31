2e24d83b648a542ce1ef9fb1ad0ab48b01b65a30
Pwersa <indaya42@gmail.com>
2022-10-31 22:13:01 +0800

    ADDED CUT PROCEDURES

--- RASPI/x.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'x.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-
-#### TESTING FOR EXPORTING UI FILE TO PY FILE
