5e0870857b3a5b08d8ce3f1a65330116d9f96151
Pwersa <indaya42@gmail.com>
2022-11-21 13:24:30 +0800

    ADDED STYLING

--- RASPI/CABINET.py
+++ RASPI/CABINET.py
@@ -302,7 +302,7 @@ class Ui_select_body_part(QMainWindow):
                 BUFFER_SIZE = 4096 # send 4096 bytes each time stepr
 
                 # the ip address or hostname of the server, the receiver
-                host = "ENTER IP HERE"
+                host = "192.168.1.6"
                 # the port, let's use 5001
                 port = 4899
                 # the name of file we want to send, make sure it exists
@@ -507,53 +507,53 @@ class Ui_gender_patient_window(QMainWindow):
         
     def send_to_companion(self):
         global dead
-        while not dead:
-            try:
-                SEPARATOR = "<SEPARATOR>"
-                BUFFER_SIZE = 4096 # send 4096 bytes each time stepr
+        
+        try:
+            SEPARATOR = "<SEPARATOR>"
+            BUFFER_SIZE = 4096 # send 4096 bytes each time stepr
 
-                # the ip address or hostname of the server, the receiver
-                host = "ENTER IP HERE"
-                # the port, let's use 5001
-                port = 4799
-                # the name of file we want to send, make sure it exists
-                filename = "cabinet-history/session/recorded_session.csv"
-                # get the file size
-                filesize = os.path.getsize(filename)
+            # the ip address or hostname of the server, the receiver
+            host = "192.168.1.6"
+            # the port, let's use 5001
+            port = 4799
+            # the name of file we want to send, make sure it exists
+            filename = "cabinet-history/session/recorded_session.csv"
+            # get the file size
+            filesize = os.path.getsize(filename)
 
-                # create the client socket
-                s = socket.socket()
+            # create the client socket
+            s = socket.socket()
 
-                print(f"[+] Connecting to {host}:{port}")
-                s.connect((host, port))
-                print("[+] Connected.")
+            print(f"[+] Connecting to {host}:{port}")
+            s.connect((host, port))
+            print("[+] Connected.")
 
-                # send the filename and filesize
-                s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+            # send the filename and filesize
+            s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
-                # start sending the file
-                progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-                with open(filename, "rb") as f:
-                    while True:
-                        
-                        # read the bytes from the file
-                        bytes_read = f.read(BUFFER_SIZE)
-                        if not bytes_read:
-                            # file transmitting is done
-                            break
-
-                        # we use sendall to assure transimission in 
-                        # busy networks
-                        s.sendall(bytes_read)
+            # start sending the file
+            progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+            with open(filename, "rb") as f:
+                while True:
                     
-                        # update the progress bar
-                        progress.update(len(bytes_read))
+                    # read the bytes from the file
+                    bytes_read = f.read(BUFFER_SIZE)
+                    if not bytes_read:
+                        # file transmitting is done
+                        break
 
-                # close the socket
-                s.close()
-                dead = True
-            except:
-                self.send_to_companion()
+                    # we use sendall to assure transimission in 
+                    # busy networks
+                    s.sendall(bytes_read)
+                
+                    # update the progress bar
+                    progress.update(len(bytes_read))
+
+            # close the socket
+            s.close()
+            dead = True
+        except:
+            self.send_to_companion()
         
         
 ####################### STEPS UI FOR EVERY INJURIES  #######################
